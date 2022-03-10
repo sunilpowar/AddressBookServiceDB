053dbc4e142c26b02b0cdaef8c0db23a455271f1
sunilpowar <sunilpowarsp8468@gmail.com>
2022-03-11 02:43:26 +0530

    Adding UC3

--- AddressBookSystemDB/AddressBookDB.sql
+++ AddressBookSystemDB/AddressBookDB.sql
@@ -18,6 +18,14 @@ Email varchar(100)
 );
 SELECT * FROM AddressBook_Table;
 
+--#UC3 Inserting Value Into The AddressBook Table
+INSERT INTO AddressBook_Table VALUES('Sourabh', 'Powar', 'Chikhali', 'Kolhapur', 'Maharashtra', 416235,9970965164, 'sourabhpowarsp8468@gamil.com'),
+						      ('Ajim', 'Maner', 'Kini', 'Hatkanangale', 'Maharashtra', 416246, 8946512348, 'ajim@gamil.com'),	
+					       	('Snehal', 'Awate', 'Mamadapur', 'Belgaum', 'Karnataka', 591238,9764581346, 'snehal@gmail.com'),	
+					     	('Sanika', 'Patil', 'Jatrat', 'Nipani', 'Karnataka',539237, 9764151348, 'sanika@gmail.com'),	
+					    	('Anil', 'Matakar', 'Gadhinglaj', 'Kolhapur', 'Maharashtra', 416784, 9764513854, 'anil@gmail.com');
+SELECT * FROM AddressBook_Table;
+
 
 
 
