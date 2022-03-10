04ceeb9a99b77641a533ae6f3c4bb67b591ea7b7
sunilpowar <sunilpowarsp8468@gmail.com>
2022-03-11 02:34:07 +0530

    Adding UC2

--- AddressBookSystemDB/AddressBookDB.sql
+++ AddressBookSystemDB/AddressBookDB.sql
@@ -4,6 +4,19 @@
 CREATE DATABASE AddressBook_ServiceDB;
 USE AddressBook_ServiceDB;
 
+--#UC2 Create AddressBook Table
+CREATE TABLE AddressBook_Table
+(
+FirstName varchar(50),
+LastName varchar(50),
+Address varchar(100),
+City varchar(20),
+State varchar (20),
+Zip bigint,
+PhoneNumber bigint,
+Email varchar(100)
+);
+SELECT * FROM AddressBook_Table;
 
 
 
